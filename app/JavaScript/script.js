9ac26230fbc5535a60a0f7d5580d9a056c4021c6
Piotr Korowicki <Piotrkorowicki@gmail.com>
2017-05-18 19:00:57 +0200

    some js cleaning + animated hamburger button

--- app/JavaScript/script.js
+++ app/JavaScript/script.js
@@ -1,56 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-//Folding mobile sub menu
-var subMenuBtn = document.querySelectorAll('.mobile_menu_add');
-var button = document.querySelectorAll('.mobile_menu_add');
-  for (var i = 0; i < button.length; i++){
+  //Folding mobile sub menu
+  let subMenuBtn = document.querySelectorAll('.mobile_menu_add');
+  let button = document.querySelectorAll('.mobile_menu_add');
+  for (var i = 0; i < button.length; i++) {
 
-    button[i].addEventListener('click',function(){
-        $(this).next().slideToggle(300);
+    button[i].addEventListener('click', function() {
+      $(this).next().slideToggle(300);
     });
   }
 
-//Slider
-  var circle1 = document.querySelector('#circle1');
-  var circle2 = document.querySelector('#circle2');
-  var sliderCircles = document.querySelectorAll('.circle')
-  var slideWrapper = document.querySelector('#slideWrapper');
+  //Slider
+  let SlideToggle = (element, classEl1, classEl2) => {
+    element.classList.remove(classEl1);
+    element.classList.add(classEl2);
+  };
 
-  circle1.addEventListener('click',function(){
-    circle2.classList.add('circle_hide');
-    circle1.classList.remove('circle_hide');
-    slideWrapper.classList.remove('slideRight');
-    slideWrapper.classList.add('slideLeft');
+  let CircleToogle = (element1, element2, classEl) => {
+    element1.classList.add(classEl);
+    element2.classList.remove(classEl)
+  };
+
+  let circle1 = document.querySelector('#circle1');
+  let circle2 = document.querySelector('#circle2');
+  let sliderCircles = document.querySelectorAll('.circle')
+  let slideWrapper = document.querySelector('#slideWrapper');
+
+  circle1.addEventListener('click', function() {
+    CircleToogle(circle2, circle1, 'circle_hide');
+    SlideToggle(slideWrapper, 'slideRight', 'slideLeft');
   });
 
-  circle2.addEventListener('click',function(){
-    circle1.classList.add('circle_hide');
-    circle2.classList.remove('circle_hide');
-    slideWrapper.classList.remove('slideLeft');
-    slideWrapper.classList.add('slideRight');
+  circle2.addEventListener('click', function() {
+    CircleToogle(circle1, circle2, 'circle_hide');
+    SlideToggle(slideWrapper, 'slideLeft', 'slideRight');
   });
+  //Switch to RWD menu
+  var burgerMenu = $('.burgerMenu');
+  var mobile_menu = $('.mobile_menu');
+  var shadow = $('.shadow');
 
-//Switch to RWD menu
-  var hamburger_button = document.querySelector('.hamburger_button');
-  var cross = document.querySelector('.cross');
-  var hamburger_menu = document.querySelector('.mobile_menu');
-  var mobile_menu = document.querySelector('.mobile_menu');
-  var shadow = document.querySelector('.shadow');
-
-    hamburger_button.addEventListener('click', function() {
-        hamburger_menu.style.left = "0";
-        hamburger_button.style.display= "none";
-        cross.style.display ="block";
-        shadow.style.visibility = "visible"
-    });
-
-    cross.addEventListener('click', function() {
-        hamburger_menu.style.left = "-400px";
-        hamburger_button.style.display= "block";
-        cross.style.display="none"
-        mobile_menu.style.boxShadow ="none";
-        shadow.style.visibility = "hidden"
-
-    });
+  burgerMenu.click(function() {
+    $(this).toggleClass('rwd');
+    mobile_menu.toggleClass('mobile_menu_hide mobile_menu_visible');
+    shadow.toggleClass('shadow_hide shadow_visible');
+  });
 
 });
